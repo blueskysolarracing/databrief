5a386685feeb13c7f226e6c6dd97b8baf0f838df
alexlewis9 <150391000+alexlewis9@users.noreply.github.com>
2025-02-06 13:54:14 -0500

    load and dump field helpers

--- databrief/utilities.py
+++ databrief/utilities.py
@@ -3,46 +3,59 @@ from typing import Any, Type
 import struct
 
 
+def _dump_field(value: Any, field_type: Type) -> bytes:
+    if issubclass(field_type, int):  # type: ignore[arg-type]
+        return struct.pack('i', value)
+    elif issubclass(field_type, float):  # type: ignore[arg-type]
+        return struct.pack('d', value)
+    elif issubclass(field_type, bool):  # type: ignore[arg-type]
+        return struct.pack('?', value)
+    elif issubclass(field_type, str):
+        encoded_string = value.encode('utf-8')
+        return struct.pack('i', len(encoded_string)) + encoded_string
+    else:
+        raise TypeError(f'Unsupported field type {field_type}')
+
+def _load_field(data: bytes, offset: int, field_type: Type) -> (Any, int):
+    if issubclass(field_type, int):  # type: ignore[arg-type]
+        value = struct.unpack_from('i', data, offset)[0]
+        offset += struct.calcsize('i')
+    elif issubclass(field_type, float):  # type: ignore[arg-type]
+        value = struct.unpack_from('d', data, offset)[0]
+        offset += struct.calcsize('d')
+    elif issubclass(field_type, bool):  # type: ignore[arg-type]
+        value = struct.unpack_from('?', data, offset)[0]
+        offset += struct.calcsize('?')
+    elif issubclass(field_type, str):
+        length = struct.unpack_from('i', data, offset)[0]
+        offset += struct.calcsize('i')
+        value = data[offset:offset + length].decode('utf-8')
+        offset += length
+    else:
+        raise TypeError(f'Unsupported field type {field_type}')
+    return value, offset
+
 def dump(instance: Any) -> bytes:
     if not is_dataclass(instance):
         raise TypeError('Dump function only accepts dataclass instances.')
 
-    int_float_values = []
+    packed_data = bytearray()
     bools = []
-    format_parts = []
-    strings = []
 
     for field in fields(instance):
         value = getattr(instance, field.name)
-
-        if issubclass(field.type, int):  # type: ignore[arg-type]
-            int_float_values.append(value)
-            format_parts.append('i')
-        elif issubclass(field.type, float):  # type: ignore[arg-type]
-            int_float_values.append(value)
-            format_parts.append('d')
-        elif issubclass(field.type, bool):  # type: ignore[arg-type]
+        if issubclass(field.type, bool):  # type: ignore[arg-type]
             bools.append(value)
-        elif issubclass(field.type, str):
-            encoded_string = value.encode('utf-8')
-            strings.append(encoded_string)
         else:
-            raise TypeError(f'Unsupported field type {field.type}')
-
-    format_string = ''.join(format_parts)
-    packed_data = struct.pack(format_string, *int_float_values)
+            packed_data.extend(_dump_field(value, field.type))
+    
     bool_bytes = bytearray()
 
     for i in range(0, len(bools), 8):
         bool_byte = sum(1 << j for j, b in enumerate(bools[i:i + 8]) if b)
         bool_bytes.append(bool_byte)
 
-    string_bytes = bytearray()
-    for encoded_string in strings:
-        string_bytes.extend(struct.pack('i', len(encoded_string)))
-        string_bytes.extend(encoded_string)
-
-    return packed_data + bytes(bool_bytes) + bytes(string_bytes)
+    return bytes(packed_data) + bytes(bool_bytes)
 
 
 def load(data: bytes, cls: Type[Any]) -> Any:
@@ -51,31 +64,14 @@ def load(data: bytes, cls: Type[Any]) -> Any:
 
     offset = 0
     field_values = {}
-    int_float_fields = []
     bool_fields = []
-    format_parts = []
-    string_fields = []
 
     for field in fields(cls):
-        if issubclass(field.type, int):  # type: ignore[arg-type]
-            int_float_fields.append(field)
-            format_parts.append('i')
-        elif issubclass(field.type, float):  # type: ignore[arg-type]
-            int_float_fields.append(field)
-            format_parts.append('d')
-        elif issubclass(field.type, bool):  # type: ignore[arg-type]
+        if issubclass(field.type, bool):  # type: ignore[arg-type]
             bool_fields.append(field)
-        elif issubclass(field.type, str):
-            string_fields.append(field)
         else:
-            raise TypeError(f'Unsupported field type {field.type}')
-
-    format_string = ''.join(format_parts)
-    int_float_values = struct.unpack_from(format_string, data, offset)
-    offset += struct.calcsize(format_string)
-
-    for field, value in zip(int_float_fields, int_float_values):
-        field_values[field.name] = value
+            value, offset = _load_field(data, offset, field.type)
+            field_values[field.name] = value
 
     bool_values = []
     bool_byte = 0
@@ -89,12 +85,5 @@ def load(data: bytes, cls: Type[Any]) -> Any:
 
     for field, value in zip(bool_fields, bool_values):
         field_values[field.name] = bool(value)
-
-    for field in string_fields:
-        length = struct.unpack_from('i', data, offset)[0]
-        offset += struct.calcsize('i')
-        value = data[offset:offset + length].decode('utf-8')
-        offset += length
-        field_values[field.name] = value
 
     return cls(**field_values)
